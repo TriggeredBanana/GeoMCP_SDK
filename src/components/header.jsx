a80a1147294d703bab066ade55f29b16f8be2e07
Daniel <mogsterdaniel@gmail.com>
2026-02-26 14:11:40 +0100

    Fixed "className"

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 export function Header() {
     return (
-        <div class="headerBar">
+        <div className="headerBar">
             <div className="logo">
                 <img src="/norkartFull.png"></img>
             </div>

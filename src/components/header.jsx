e95f979a4ca3348680efcf4749dd086ca1abb750
Daniel <mogsterdaniel@gmail.com>
2026-02-26 14:01:37 +0100

    Added Norkart logo to header

---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+        <div class="headerBar">
+            <div className="logo">
+                <img src="/norkartFull.png"></img>
+            </div>
+        </div>
+    )
+}

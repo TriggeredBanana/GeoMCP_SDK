d7f8e52580633f6197dbbb211d9e39d68783ae4b
Daniel <mogsterdaniel@gmail.com>
2026-02-27 09:25:32 +0100

    Add react-mui-sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import './App.css'
+import { Sidebar, Menu, MenuItem, Submenu } from 'react-mui-sidebar';
+import { Link } from 'react-router-dom';
+import {Header} from './components/header'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquarePollVertical } from '@fortawesome/free-solid-svg-icons';
+import { faMessage } from '@fortawesome/free-regular-svg-icons';
 
 function App() {
   return (
-    <div className="app">
-      {/* Website content goes here */}
-    </div>
+    <>
+      <Header />
+      <Sidebar width={"275px"} showProfile={false} themeColor="#26BE76" secondaryColor="#67CD99">
+        <Menu subHeading="Meny">
+          <MenuItem
+            icon={<FontAwesomeIcon icon={faMessage} />}
+            component={Link}
+            link="/dashboard"
+            badge={true}
+            badgeContent="7"
+            isSelected={true}
+            >Chatbot</MenuItem>
+            <MenuItem icon={<FontAwesomeIcon icon ={faSquarePollVertical} />}>Kartlag</MenuItem>
+            <MenuItem icon={<FontAwesomeIcon icon ={faSquarePollVertical} />}>Analyse</MenuItem>
+            <MenuItem icon={<FontAwesomeIcon icon ={faSquarePollVertical} />}>Eksporter</MenuItem>
+        </Menu>
+      </Sidebar>
+    </>
   )
 }
 

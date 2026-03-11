fe1c67f9395595e3e108b57f0209844f6436b5fe
Tobiasee16 <engedaltobias@icloud.com>
2026-03-11 09:46:44 +0100

    light/dark-mode switch
    added light/dark-mode switch in Header.jsx
    Added light mode that overrides dark mode when switched on in App.css
    Added functionc for light/dark-mode switch in App.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import './index.css'
 import App from './App.jsx'
 import 'leaflet/dist/leaflet.css';
